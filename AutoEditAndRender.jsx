044e3b0a29a02344c77c5e39f25ebbbddde430cf
Nazar-Artemenkov <127849488+Nazar-Artemenkov@users.noreply.github.com>
2025-11-10 14:47:47 +0200

    Update AutoEditAndRender.jsx

--- AutoEditAndRender.jsx
+++ AutoEditAndRender.jsx
@@ -1,7 +1,7 @@
 (function () {
     app.beginUndoGroup("Auto Edit and Render");
 
-    //Пошук композиції по імені.
+    // Пошук композиції по імені
     function findComp(name) {
         for (var i = 1; i <= app.project.numItems; i++) {
             var it = app.project.item(i);
@@ -10,11 +10,11 @@
         return null;
     }
 
-    //Заміна тексту в всіх слоях композиції
+    // Заміна тексту в всіх слоях композиції
     function replaceTextInComp(comp) {
         for (var i = 1; i <= comp.numLayers; i++) {
             var lyr = comp.layer(i);
-            if (lyr.matchName === "ADBE Text Layer"){
+            if (lyr.matchName === "ADBE Text Layer") {
                 var txtProp = lyr.property("Source Text");
                 if (txtProp) {
                     var td = txtProp.value;
@@ -25,36 +25,37 @@
         }
     }
 
-    //Імпорт відео і вставка в перекомпозицію
-    function importAndFitVideo(compName, videoFile){
+    // Імпорт відео і вставка в перекомпозицію
+    function importAndFitVideo(compName, videoFile) {
         var c = findComp(compName);
         if (!c) return;
 
-        //Видаляєм старі слої
+        // Видаляєм старі слої
         for (var i = c.numLayers; i >= 1; i--) c.layer(i).remove();
 
-        //імпортуєм відео
+        // Імпортуєм відео
         var footage = app.project.importFile(new ImportOptions(videoFile));
         var lyr = c.layers.add(footage);
 
-        //вираховуємо масштаб
+        // Вираховуємо масштаб
         var scaleX = (c.width / footage.width) * 100;
         var scaleY = (c.height / footage.height) * 100;
         lyr.property("Scale").setValue([scaleX, scaleY]);
     }
 
-    //Знаходимо основні композиції
+    // Знаходимо основні композиції
     var compRender = findComp("Render");
     var compCustomize = findComp("Customize Scene");
     if (!compRender || !compCustomize) {
-        alert("No Render or Customize Scene compositions found!")
+        alert("No Render or Customize Scene compositions found!");
+        app.endUndoGroup();
         return;
     }
 
-    //
+    // Пошук зв'язків
     var linkedLayers = [];
-    function scanExpressions(comp, targetComp){
-        for (var i = 1; i <= comp.numLayers; i++){
+    function scanExpressions(comp, targetComp) {
+        for (var i = 1; i <= comp.numLayers; i++) {
             var layer = comp.layer(i);
             function walk(group) {
                 for (var j = 1; j <= group.numProperties; j++) {
@@ -64,7 +65,7 @@
                             linkedLayers.push({
                                 layer: layer.name,
                                 expr: prop.expression
-                            })
+                            });
                         }
                     }
                     if (prop.numProperties > 0) walk(prop);
@@ -75,31 +76,34 @@
     }
 
     scanExpressions(compRender, compCustomize);
-    $.writeIn("Found connections: " + linkedLayers.length);
+    $.writeln("Found connections: " + linkedLayers.length);
 
-    //
-    replaceTextInComp(compRender);
-    replaceTextInComp(compCustomize);
-
-    //
-    var basePath = Folder.selectDialog("Select the folder where the videos are located")
+    // Вибір папки з відео
+    var basePath = Folder.selectDialog("Select the folder where the videos are located");
     if (!basePath) {
         alert("Video selection deselected");
+        app.endUndoGroup();
         return;
     }
+
+    // Замінюємо текст і вставляємо відео
+    replaceTextInComp(compRender);
+    replaceTextInComp(compCustomize);
 
     importAndFitVideo("Video 1", File(basePath.fsName + "/Video 1.mp4"));
     importAndFitVideo("Video 2", File(basePath.fsName + "/Video 2.mp4"));
     importAndFitVideo("Video 3", File(basePath.fsName + "/Video 3.mp4"));
 
-    //
+    app.endUndoGroup();
+
+    // Тепер вибір папки для рендера
     var outFolder = Folder.selectDialog("Select a folder to render");
     if (outFolder) {
         var rqItem = app.project.renderQueue.items.add(compRender);
         rqItem.outputModule(1).file = new File(outFolder.fsName + "/final_output.mov");
-        app.project.renderQueue.render();
-        alert("Render complete!")
-    }
 
-    app.endUndoGroup();
+        app.project.renderQueue.render();
+
+        alert("Render complete!");
+    }
 })();
